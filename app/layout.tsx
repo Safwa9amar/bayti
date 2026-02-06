4994dc2fcf7ccdd31220de1532a42b3e5c51becb
Safwa9amar <hassanih97@gmail.com>
2026-02-06 20:19:53 +0100

    Please provide the diff for `/home/astro/bayti/app/layout.tsx` to generate a commit message.

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,9 +69,36 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
           rel="stylesheet"
         />
-        {/* Facebook Pixel temporarily removed for debugging 404 error */}
+        {/* Meta Pixel */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1996891180857184');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body className="antialiased bg-off-white text-charcoal transition-colors duration-300">
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1996891180857184&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         {children}
       </body>
     </html>
